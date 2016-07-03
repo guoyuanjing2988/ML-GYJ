deb582b5e7997e44a4f520488489f7f55d7a3e55
Yuanjing Guo <Yuanjing Guo>
2016-07-03 17:29:47 +0800

    crawl all data from a year

--- NBA-predict/src/nba_crawler/NBADataCrawler.py
+++ NBA-predict/src/nba_crawler/NBADataCrawler.py
@@ -1,6 +1,9 @@
 from util.http_util import readContentFromURL
-
+import datetime
+import pickle
 nba_gameline_url='http://www.nba.com/gameline/'
+
+
 def getNumberOfGamesAtOneDay(game_date):
     url_at_game_date=nba_gameline_url+game_date+'/'
     website_content=readContentFromURL(url_at_game_date)
@@ -22,7 +25,13 @@ def getTeamsPlayingAtOneDay(game_date):
         teams_playing[-1]['away_team']=website_content[away_team_loc+39:away_team_loc+42]
         home_team_loc = website_content.find('<h5 class="nbaModTopTeamName hometeam">', prev_loc)
         teams_playing[-1]['home_team']=website_content[home_team_loc+39:home_team_loc+42]
-        prev_loc = website_content.find('<div id="nbaGL00215', home_team_loc) + 1
+        prev_loc = home_team_loc+1
+    i=0
+    while (i<len(teams_playing)-1):
+        if teams_playing[i]['home_team']==teams_playing[i+1]['home_team']:
+            del(teams_playing[i])
+        else:
+            i+=1
     return teams_playing
 
 def getURLForOneMatchData(game_date,away_team,home_team):
@@ -31,9 +40,15 @@ def getURLForOneMatchData(game_date,away_team,home_team):
 def _getScoreFromPosition(website_content,location):
     score_end_location = website_content.find('<', location + 1)
     if website_content[location+20]!='"':
-        return int(website_content[location+25:score_end_location])
+        try:
+            return int(website_content[location+25:score_end_location])
+        except:
+            return -1
     else:
-        return int(website_content[location+22:score_end_location])
+        try:
+            return int(website_content[location+22:score_end_location])
+        except:
+            return -1
 
 def getMatchScoreFromURL(match_url):
     website_content = readContentFromURL(match_url)
@@ -43,3 +58,89 @@ def getMatchScoreFromURL(match_url):
     home_team_loc=website_content.find('<h2 class="teamHome')
     score['home_team']=_getScoreFromPosition(website_content,home_team_loc)
     return score
+
+def _getColumnInformation(row_html):
+    row=[]
+    prev=0
+    while row_html.find('<td',prev)!=-1:
+        column_start=row_html.find('<td',prev)
+        column_end=row_html.find('</td>',prev)
+        row.append(row_html[column_start+4:column_end])
+        prev=column_end+1
+    return row
+
+def _getTableInformation(players_table_html):
+    table = []
+    prev = 0
+    while players_table_html.find('<tr', prev) != -1:
+        table_row_start = players_table_html.find('<tr', prev)
+        table_row_end = players_table_html.find('</tr>', table_row_start)
+        table.append(_getColumnInformation(players_table_html[table_row_start:table_row_end]))
+        prev = table_row_end + 1
+    return table
+
+def _getHealthPlayersFromHTML(players_table_html):
+    table=_getTableInformation(players_table_html)
+    health_players=[]
+    for i in range(3,len(table)):
+        player_name_start=table[i][0].find('<a href="/playerfile/')
+        if player_name_start==-1:
+            break
+        player_name_end=table[i][0].find('/index.html')
+        player_name=table[i][0][player_name_start+21:player_name_end]
+        if len(table[i][0])>2:
+            health_players.append(player_name)
+        else:
+            if table[i][1].find("DNP - Coach's Decision")!=-1:
+                health_players.append(player_name)
+    return health_players
+
+def getHealthyPlayersFromURL(match_url):
+    website_content = readContentFromURL(match_url)
+    players={}
+    away_team_loc_start=website_content.find('<table id="nbaGITeamStats"')
+    away_team_loc_end=website_content.find('</table>',away_team_loc_start)
+    players['away_team']=_getHealthPlayersFromHTML(website_content[away_team_loc_start:away_team_loc_end])
+    home_team_loc_start=website_content.find('<table id="nbaGITeamStats"',away_team_loc_end)
+    home_team_loc_end=website_content.find('</table>',home_team_loc_start)
+    players['home_team']=_getHealthPlayersFromHTML(website_content[home_team_loc_start:home_team_loc_end])
+    return players
+
+def _filltwoposition(intx):
+    if intx<10:
+        return '0'+str(intx)
+    else:
+        return str(intx)
+
+def getAllMatches(start_date,end_date):
+    matches=[]
+    for deltadate in range((end_date-start_date).days+1):
+        date_string=str((start_date+datetime.timedelta(deltadate)).year)+_filltwoposition((start_date+datetime.timedelta(deltadate)).month)+_filltwoposition((start_date+datetime.timedelta(deltadate)).day)
+        print(start_date+datetime.timedelta(deltadate))
+        teams_playing=getTeamsPlayingAtOneDay(date_string)
+        for i in range(len(teams_playing)):
+            matches.append({})
+            matches[-1]['home_team']=teams_playing[i]['home_team']
+            matches[-1]['away_team']=teams_playing[i]['away_team']
+            matches[-1]['year']=int(date_string[:4])
+            matches[-1]['month']=int(date_string[4:6])
+            matches[-1]['day']=int(date_string[6:8])
+            score=getMatchScoreFromURL(getURLForOneMatchData(date_string,teams_playing[i]['away_team'],teams_playing[i]['home_team']))
+            matches[-1]['home_team_score']=score['home_team']
+            matches[-1]['away_team_score']=score['away_team']
+            players=getHealthyPlayersFromURL(getURLForOneMatchData(date_string,teams_playing[i]['away_team'],teams_playing[i]['home_team']))
+            matches[-1]['home_team_players']=players['home_team']
+            matches[-1]['away_team_players']=players['away_team']
+            if score['home_team']==-1 or score['away_team']==-1:
+                del(matches[-1])
+    return matches
+
+def refreshNBADataPickle():
+    matches=getAllMatches(datetime.date(year=2015,month=10,day=27),datetime.date(year=2016,month=6,day=19))
+    pickle.dump(matches,open('nba_matches.pickle','wb'))
+
+
+if __name__=='__main__':
+    refreshNBADataPickle()
+
+
