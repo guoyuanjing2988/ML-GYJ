a1100d9d5af6346b829e47987ad146ce8b7daf8c
Guo <I329397@global.corp.sap>
2016-07-07 09:13:19 +0800

    fix test bug

--- NBA-predict/test/nba_crawler/testNBADataCrawler.py
+++ NBA-predict/test/nba_crawler/testNBADataCrawler.py
@@ -12,7 +12,7 @@ class TestNBADataCrawler(unittest.TestCase):
         result=getTeamsPlayingAtOneDay('20151025')
         self.assertEqual(result,[])
         result=getTeamsPlayingAtOneDay('20160302')
-        self.assertEqual(result,[{'home_team': 'ORL', 'away_team': 'CHI'}, {'home_team': 'PHI', 'away_team': 'CHA'}, {'home_team': 'BOS', 'away_team': 'POR'}, {'home_team': 'TOR', 'away_team': 'UTA'}, {'home_team': 'HOU', 'away_team': 'NOP'}, {'home_team': 'MEM', 'away_team': 'SAC'}, {'home_team': 'MIL', 'away_team': 'IND'}, {'home_team': 'MIN', 'away_team': 'WAS'}, {'home_team': 'SAS', 'away_team': 'DET'}, {'home_team': 'DEN', 'away_team': 'LAL'}])
+        self.assertEqual(result,[{'home_team': 'ORL', 'away_team': 'CHI'}, {'home_team': 'PHI', 'away_team': 'CHA'}, {'home_team': 'BOS', 'away_team': 'POR'}, {'home_team': 'TOR', 'away_team': 'UTA'}, {'home_team': 'HOU', 'away_team': 'NOP'}, {'home_team': 'MEM', 'away_team': 'SAC'}, {'home_team': 'MIL', 'away_team': 'IND'}, {'home_team': 'MIN', 'away_team': 'WAS'}, {'home_team': 'SAS', 'away_team': 'DET'}, {'home_team': 'DEN', 'away_team': 'LAL'}, {'away_team': 'OKC', 'home_team': 'LAC'}])
 
     def testGetURLForOneMatchData(self):
         result=getURLForOneMatchData('20160203','CLE','CHA')
