a5e482874920536d535dd0d03fa2f2d39f8ecd25
Guo <I329397@global.corp.sap>
2016-07-11 10:20:29 +0800

    feature extractor

--- NBA-predict/test/nba_feature_generator/testNBAFeatureExtractor.py
+++ NBA-predict/test/nba_feature_generator/testNBAFeatureExtractor.py
@@ -1,8 +1,12 @@
 import unittest
-from nba_feature_generator.NBAFeatureExtractor import generateVectorForPlayersInOneTeam
+from nba_feature_generator.NBAFeatureExtractor import generateVectorForPlayersInOneTeam,generateVectorForOneTeamName
 
 class TestNBAFeatureExtractor(unittest.TestCase):
     def testGenerateVectorForPlayersInOneTeam(self):
         result=generateVectorForPlayersInOneTeam(['kawhi_leonard', 'lamarcus_aldridge', 'tim_duncan', 'daniel_green', 'tony_parker', 'emanuel_ginobili', 'boris_diaw', 'kyle_anderson', 'patrick_mills', 'david_west', 'jonathon_simmons', 'matt_bonner', 'boban_marjanovic'])
         self.assertEqual(result[270],0)
 
+    def testGenerateVectorForOneTeamName(self):
+        result=generateVectorForOneTeamName('CLE')
+        self.assertEqual(result[20],1)
+        self.assertEqual(sum(result),1)
